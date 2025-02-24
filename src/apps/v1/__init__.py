eb610c84cf389cf07a1596d3fdfc3a67367d1a80
aaronzhou79 <aaron.zhou@gmail.com>
2025-02-24 09:26:45 +0800

    refactor(v1): 移除代码生成相关功能
    - 删除了 code_gen 目录下的所有文件
    - 更新了 __init__.py 中的路由注册顺序
    - 在 healthy.py 中添加了健康检查路由的标签
    - 在 cspell.json 中添加了 aiocache 词汇

--- src/apps/v1/__init__.py
+++ src/apps/v1/__init__.py
@@ -9,12 +9,12 @@
 
 from fastapi import APIRouter
 
-from .code_gen.api import router as code_gen_router
+# from .code_gen import router as code_gen_router
 from .init_data.api import router as init_data_router
 from .sys import router as sys_router
 
 router = APIRouter(prefix="/v1")
 
-router.include_router(code_gen_router)
-router.include_router(init_data_router)
+# router.include_router(code_gen_router)
 router.include_router(sys_router)
+router.include_router(init_data_router)
